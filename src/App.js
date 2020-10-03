1e05cdb6ccf0e6a619a98534eb1b85462502e3a3
FrancoRosa <km115.franco@gmail.com>
2020-10-03 17:38:06 -0500

    test prop-types

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import Big from 'big.js';
+import PropTypes from 'prop-types';
 import './App.css';
 
-function App() {
+const MyComponent = props => {
+  const { children } = props;
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+MyComponent.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+const App = () => {
   const x = new Big(123.4567);
   const y = Big('123456.7e-3');
   const z = new Big(x);
   return (
     <div className="app">
       <h2>Empty project</h2>
-      <span>
+      <p>
         Is Big working?:
         <strong>{ String(x.eq(y) && x.eq(z) && y.eq(z)) }</strong>
-      </span>
+      </p>
+      <p>
+        Is PropTypes working?:
+        <strong>{ String(true) }</strong>
+      </p>
     </div>
   );
-}
+};
 
 export default App;
