44fdbea75e9e3f1fefe1062a7d403d1078173fd3
FrancoRosa <km115.franco@gmail.com>
2020-10-03 22:16:08 -0500

    Add button groups

--- src/components/App.js
+++ src/components/App.js
@@ -35,11 +35,30 @@ const ButtonPanel = props => {
   return (
     <div>
       <div>
-        <Button button='1' />
-        <Button button='2' />
-        <Button button='3' />
-        <Button button='4' />
+        <Button button='AC' />
+        <Button button='+/-' />
+        <Button button='%' />
+        <Button button='÷' />
       </div>
+      <div>
+        <Button button='7' />
+        <Button button='8' />
+        <Button button='9' />
+        <Button button='X' />
+      </div>
+      <div>
+        <Button button='4' />
+        <Button button='5' />
+        <Button button='6' />
+        <Button button='-' />
+      </div>
+      <div>
+        <Button button='0' />
+        <Button button='.' />
+        <Button button='=' />
+        <Button button='?' />
+      </div>
+
     </div>
   );
 };
