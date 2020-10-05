80fb52107a63dea271b7cbbf2c256c8409f4b715
FrancoRosa <km115.franco@gmail.com>
2020-10-05 12:21:50 -0500

    Implemented first calculate case

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,3 +1,21 @@
-const calculate = ({total, next, operation}, buttonName) => {
-  
+const calculate = ({ total, next, operation }, buttonName) => {
+  const result = {
+    total,
+    next,
+    operation,
+  };
+
+  switch (buttonName) {
+    case '+/-':
+      result.total *= -1;
+      result.next *= -1;
+      break;
+
+    default:
+      break;
+  }
+
+  return result;
 }
+
+export default calculate;
