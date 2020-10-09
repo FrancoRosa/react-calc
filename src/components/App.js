7299b6b49f540f581c0b5123036c7394e6c75a54
FrancoRosa <km115.franco@gmail.com>
2020-10-09 18:50:17 -0500

    Make App stateful

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
+import calculate from '../logic/calculate';
 import '../styles/App.css';
-// import calculate from '../logic/calculate';
 
-const App = () => (
-  <div className="app">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
 
+  handleClick(buttonName) {
+    const result = calculate(buttonName);
+    this.setState({
+      total: result.total,
+      next: result.next,
+      operation: result.operation,
+
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
+
+}
 export default App;
