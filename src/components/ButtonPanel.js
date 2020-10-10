135cfab59c536f930882f427845858bae9b8e4c5
FrancoRosa <km115.franco@gmail.com>
2020-10-09 19:03:02 -0500

    Pass props to children

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,36 +2,40 @@ import React from 'react';
 import Button from './Button';
 import '../styles/ButtonPanel.css';
 
+const handleClick = (props, text) => {
+  return props.clickHandler(text);
+};
+
 const ButtonPanel = () => (
   <div id="button-panel">
     <div>
-      <Button text="AC" color="#e0e0e0" />
-      <Button text="+/-" color="#e0e0e0" />
-      <Button text="%" color="#e0e0e0" />
-      <Button text="÷" />
+      <Button clickHandler={handleClick} text="AC" color="#e0e0e0" />
+      <Button clickHandler={handleClick} text="+/-" color="#e0e0e0" />
+      <Button clickHandler={handleClick} text="%" color="#e0e0e0" />
+      <Button clickHandler={handleClick} text="÷" />
     </div>
     <div>
-      <Button text="7" color="#e0e0e0" />
-      <Button text="8" color="#e0e0e0" />
-      <Button text="9" color="#e0e0e0" />
-      <Button text="x" />
+      <Button clickHandler={handleClick} text="7" color="#e0e0e0" />
+      <Button clickHandler={handleClick} text="8" color="#e0e0e0" />
+      <Button clickHandler={handleClick} text="9" color="#e0e0e0" />
+      <Button clickHandler={handleClick} text="x" />
     </div>
     <div>
-      <Button text="4" color="#e0e0e0" />
-      <Button text="5" color="#e0e0e0" />
-      <Button text="6" color="#e0e0e0" />
-      <Button text="-" />
+      <Button clickHandler={handleClick} text="4" color="#e0e0e0" />
+      <Button clickHandler={handleClick} text="5" color="#e0e0e0" />
+      <Button clickHandler={handleClick} text="6" color="#e0e0e0" />
+      <Button clickHandler={handleClick} text="-" />
     </div>
     <div>
-      <Button text="1" color="#e0e0e0" />
-      <Button text="2" color="#e0e0e0" />
-      <Button text="3" color="#e0e0e0" />
-      <Button text="+" />
+      <Button clickHandler={handleClick} text="1" color="#e0e0e0" />
+      <Button clickHandler={handleClick} text="2" color="#e0e0e0" />
+      <Button clickHandler={handleClick} text="3" color="#e0e0e0" />
+      <Button clickHandler={handleClick} text="+" />
     </div>
     <div>
-      <Button text="0" color="#e0e0e0" wide />
-      <Button text="." color="#e0e0e0" />
-      <Button text="=" />
+      <Button clickHandler={handleClick} text="0" color="#e0e0e0" wide />
+      <Button clickHandler={handleClick} text="." color="#e0e0e0" />
+      <Button clickHandler={handleClick} text="=" />
     </div>
   </div>
 );
